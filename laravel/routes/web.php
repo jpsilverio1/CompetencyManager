cc3195bb8441286b90b7f968b1d338394c64a1aa
Jessica Silverio <jpsilverio1@gmail.com>
2017-06-05 03:36:51 -0400

    fixing route and adding jquery files to the app layout

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/user-competences', 'UserController@addCompetences');
 Route::get("/jessica", function(){
     return View::make("jessica");
 });
