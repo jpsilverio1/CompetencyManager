e83120cb90e0fb4858ae1eb86ca3b184c96e8929
Jessica Silverio <jpsilverio1@gmail.com>
2017-07-03 00:33:25 -0400

    bug fix for user profile

--- laravel/resources/views/users/profile.blade.php
+++ laravel/resources/views/users/profile.blade.php
@@ -25,13 +25,13 @@
 			<div class="panel panel-default">
                     <div class="panel-heading" >
                         Tarefas criadas por este usuário
-                        @include('tasks.show_paginated_tasks', ['tasks' => Auth::user()->createdTasks()->paginate(10, ['*'],'tasks'), 'noTasksMessage' => 'Não há tarefas para exibição.'])
+                        @include('tasks.show_paginated_tasks', ['tasks' => $user->createdTasks()->paginate(10, ['*'],'tasks'), 'noTasksMessage' => 'Não há tarefas para exibição.'])
                     </div>
                 </div>
                 <div class="panel panel-default">
                     <div class="panel-heading" >
                         Equipes das quais o usuário faz parte
-                        @include('teams.show_paginated_teams', ['teams' => Auth::user()->teams()->paginate(10, ['*'],'teams')])
+                        @include('teams.show_paginated_teams', ['teams' => $user->teams()->paginate(10, ['*'],'teams')])
                     </div>
                 </div>
 
