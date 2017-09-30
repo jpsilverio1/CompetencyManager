fc33f0f9fb02434dac5d94c5d3865586e885c60c
mendesrafac <mendesrafa.c@gmail.com>
2017-09-30 17:52:42 -0300

    adding show file at learningaids views

--- laravel/resources/views/learningaids/show.blade.php
+++ laravel/resources/views/learningaids/show.blade.php
@@ -4,7 +4,7 @@
         <div class="panel panel-default">
             <div class="panel-heading text-center text-capitalize" >
                 <h2>
-                    {{$competence->name}}
+                    {{$learningaid->name}}
                 </h2>
             </div>
             <div class="panel-body">
@@ -16,34 +16,38 @@
                <h4>
                    Descrição
                </h4>
-                <p>{{$competence->description}}</p>
+                <p>{{$learningaid->description}}</p>
                 <div class="panel panel-default">
                     <div class="panel-heading" >
-                            Usuários que possuem a competência
-                        @include('users.show_paginated_users', ['users' => $competence->skilledUsers()->paginate(10, ['*'],'users')])
+                            Usuários indicados
+                        @include('users.show_paginated_users', ['users' => $learningaid->unskilledUsers()->paginate(10, ['*'],'users')])
+                    </div>
+                </div>
+                {{--<div class="panel panel-default">
+                    <div class="panel-heading" >
+                        Tarefas pendentes
+                        {{--@include('tasks.show_paginated_tasks', ['tasks' => $learningaid->tasksThatRequireIt()->paginate(10, ['*'],'tasks'), 'noTasksMessage' => 'Não há tarefas para exibição.'])
                     </div>
                 </div>
                 <div class="panel panel-default">
                     <div class="panel-heading" >
-                        Tarefas que necessitam desta competência
-                        @include('tasks.show_paginated_tasks', ['tasks' => $competence->tasksThatRequireIt()->paginate(10, ['*'],'tasks'), 'noTasksMessage' => 'Não há tarefas para exibição.'])
+                        Equipes pendentes
+                        {{--@include('teams.show_paginated_teams', ['teams' => $learningaid->teamsThatHaveIt()->paginate(10, ['*'],'teams')])
                     </div>
-                </div>
-                <div class="panel panel-default">
-                    <div class="panel-heading" >
-                        Equipes que possuem esta competência
-                        @include('teams.show_paginated_teams', ['teams' => $competence->teamsThatHaveIt()->paginate(10, ['*'],'teams')])
-                    </div>
-                </div>
+                </div>--}}
                     <div>
-                        <div class="col-md-2">
-                            <a href='{{ route('competences.edit', $competence->id) }}'/><button type="submit" class="btn btn-primary">Editar Competência</button>
+                        <div class="col-md-1">
+                            <a href='{{ route('learningaids.edit', $learningaid->id) }}'/>
+                            <button type="submit" class="btn btn-primary">Editar</button>
                         </div>
                         <div>
-                            <form  id="deleteCompetencesForm" role="form" method="POST" action="{{ route('competences.destroy', $competence->id ) }}">
+                            <form id="deleteLearningAidsForm" role="form" method="POST"
+                                  action="{{ route('learningaids.destroy', $learningaid->id ) }}">
                                 {{ csrf_field() }}
-                                <input type="hidden" name="_method" value="DELETE" />
-                                <td><button type="" class="btn btn-danger">Excluir Competência</button></td>
+                                <input type="hidden" name="_method" value="DELETE"/>
+                                <td>
+                                    <button type="" class="btn btn-danger">Excluir</button>
+                                </td>
                             </form>
                         </div>
 
