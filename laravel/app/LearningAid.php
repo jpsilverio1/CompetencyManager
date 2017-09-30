77628277cce13dd824225077fa03770de8baa920
mendesrafac <mendesrafa.c@gmail.com>
2017-09-30 17:44:48 -0300

    including unskilledUsers() method

--- laravel/app/LearningAid.php
+++ laravel/app/LearningAid.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class LearningAid extends Model
 {
     //
+    protected $table = 'learningaids';
+
     protected $fillable = [
         'name', 'description',
     ];
+
+    public function unskilledUsers() {
+        return $this->belongsToMany('App\User', 'learningaids_user', 'learningaids_id', 'user_id');
+    }
 
     public function competencies()
     {
