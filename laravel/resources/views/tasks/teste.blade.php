c72d3ac3df8ad5e4a649a4a724801510d97f9d69
mendesrafac <mendesrafa.c@gmail.com>
2017-10-08 22:42:32 -0300

    modifying teste view for a better table placement

--- laravel/resources/views/tasks/teste.blade.php
+++ laravel/resources/views/tasks/teste.blade.php
@@ -9,28 +9,20 @@
 
 
     <!-- Simple List -->
-    <ul id="teamCandidates" class="list-group col-md-2">
-        @if (count($task->allCandidates()) > 0)
-            @foreach ($task->allCandidates() as $candidate)
-                <li class="list-group-item">{{$candidate->name}}</li>
-            @endforeach
-        @endif
-    </ul>
-    <div  class="col-md-2" style="display:block; -webkit-padding-start: 40px;" >
-        <div class="layer title">List B</div>
-        <ul id="candidateTeam" class="list-group col-md-4">
-
-
-
-
-
-
-
-
+    <div style="text-align:center;" class="col-md-8 col-md-offset-2">
+        <div class="layer title col-md-4 col-md-offset-1">List A</div>
+        <div class="layer title col-md-4 col-md-offset-1">List B</div>
+        <ul id="teamCandidates" class="list-group col-md-4 col-md-offset-1" style="background-color: white; min-height: 40px;max-width: 50%; padding:0;">
+            @if (count($task->allCandidates()) > 0)
+                @foreach ($task->allCandidates() as $candidate)
+                    <li class="list-group-item">{{$candidate->name}}</li>
+                @endforeach
+            @endif
         </ul>
-    </div>
-    
+        <ul id="candidateTeam" class="list-group col-md-4 col-md-offset-1" style="background-color: white; min-height: 40px;max-width: 50%; padding:0;">
+        </ul>
+    </div >
+
 
     <script src="{{ asset('js/team-task-recomendation.js') }}"></script>
 @endsection
-
