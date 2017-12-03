a1b3f69758a368166b6aeb0ae594355d129e2443
mendesrafac <mendesrafa.c@gmail.com>
2017-12-03 17:15:41 -0200

    changing db route

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'compmanager.mysql.uhserver.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'homestead'),
-            'username' => env('DB_USERNAME', 'homestead'),
-            'password' => env('DB_PASSWORD', 'secret'),
+            'database' => env('DB_DATABASE', 'compmanager'),
+            'username' => env('DB_USERNAME', 'tcchjr'),
+            'password' => env('DB_PASSWORD', 'CChjrmsc17*!'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
