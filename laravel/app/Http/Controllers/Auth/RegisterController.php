93bd94ea1d0d512daaee05725d40756acd7597a9
HectorNM <hectornmelo@gmail.com>
2018-02-25 14:02:14 -0300

    refactor 1: increment user when new user registered; there's no decrement

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\DB;
 
 class RegisterController extends Controller
 {
@@ -63,6 +64,8 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+		\DB::table('basic_statistics')->where('name', 'users_count')->increment('value');
+		
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
