067a415a2f56e84ff279e0d7f55cc35a7b0de627
mendesrafac <mendesrafa.c@gmail.com>
2018-02-25 16:06:11 -0300

    adding notifications button

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -97,6 +97,25 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Registro</a></li>
                         @else
+
+                           <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    <i class="glyphicon glyphicon-bell"></i>
+                                    <span class="badge badge-light">{{auth()->user()->unreadNotifications->count()}}</span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    @if(auth()->user()->notifications->count() == 0)
+                                        <li><a>Sem notificações</a></li>
+                                    @else
+                                        @foreach(auth()->user()->notifications as $notification)
+                                            <li><a href="">{{$notification->data['data']}}</a></li>
+                                        @endforeach
+                                    @endif
+                                </ul>
+                            </li>
+
+
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
