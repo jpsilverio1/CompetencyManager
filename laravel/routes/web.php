e4048786f1804461c0c740581f9a23cb4cf26c59
HectorNM <hectornmelo@gmail.com>
2018-03-10 13:04:17 -0300

    fix in routes: using laravel route function

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -24,7 +24,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 /* User has to be authenticated to acess all of the routes listed below*/
 Route::group(['middleware' => 'auth'], function() {
 	
-	Route::get('tasks/show_form/{taskId}', 'TaskController@showForm');
+	Route::get('tasks/show_form/{taskId}', 'TaskController@showForm')->name('show-task-form');
 	
     Route::resource('tasks', 'TaskController');
     //Route::resource('teams', 'TeamController');
