8fa6a13aeda2370665e3a039c03e0bee725462e9
Jessica Silverio <jpsilverio1@gmail.com>
2018-09-16 10:28:12 -0400

    Fixing feasible tasks piechart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -52,8 +52,13 @@ class DashboardController extends Controller
 		$feasible_tasks_pie_chart = \Lava::DataTable();
 		$feasible_tasks_pie_chart->addStringColumn('Tarefas');
 		$feasible_tasks_pie_chart->addNumberColumn('Porcentagem');
-		$feasible_tasks_pie_chart->addRow(['Executáveis',$feasible_tasks_count]);
-		//$feasible_tasks_pie_chart->addRow(['Não-executáveis',$not_feasible_tasks_count]);
+		if ($feasible_tasks_count > 0) {
+            $feasible_tasks_pie_chart->addRow(['Executáveis',$feasible_tasks_count]);
+        }
+		if ($not_feasible_tasks_count > 0) {
+            $feasible_tasks_pie_chart->addRow(['Não-executáveis',$not_feasible_tasks_count]);
+        }
+
 		
 		return \Lava::PieChart('feasible_tasks_pie_chart', $feasible_tasks_pie_chart, [
 			'title' => 'Gráfico de Tarefas Executáveis vs Não-Executáveis',
