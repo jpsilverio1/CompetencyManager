c46346c5657b6874a4ee29fe82bba3e8c309ced6
Jessica Silverio <jpsilverio1@gmail.com>
2019-01-11 00:23:11 -0500

    cleaned code for hierarchical seeding

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,22 +37,15 @@ class HomeController extends Controller
     public function testa()
     {
         $last_indentation = 0;
-        $stack = [];
         $database_id_stack = [];
         foreach(file(base_path('resources/assets/seeds/teste_seeding.txt')) as $competenceName) {
             $i = substr_count($competenceName,'	');
-            echo "que coisa doida $competenceName $i <br>";
             if ($i<=$last_indentation) {
                 $most = ($last_indentation - $i)+1;
-                $stackSize = count($stack);
-                echo " status = $most / $stackSize <br>";
                 for ($j = 0; $j < $most; $j++) {
-                    $elem = array_shift($stack);
-                    $elem_id = array_shift($database_id_stack);
-                    echo "removendo $elem <br>";
+                    array_shift($database_id_stack);
                 }
             }
-            $parent = $this->array_peek($stack);
             $parent_id = $this->array_peek($database_id_stack);
             //save to database
             $competence = new \App\Competency;
@@ -64,14 +57,6 @@ class HomeController extends Controller
             $competence->save();
             $database_id = $competence->id;
             array_unshift($database_id_stack,$database_id);
-            array_unshift($stack, $competenceName);
-            echo " ------------------------------------ <br>";
-            echo "nozao: $competenceName" ;
-            echo " || i= $i || last_id = $last_indentation || parent: $parent || parent database id= $parent_id || database id= $database_id || current stack: <br>";
-            print_r($stack);
-
-            echo "<br>";
-
             $last_indentation = $i;
         }
     }
