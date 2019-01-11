0ab4970ae87a33b13ccb85a4e02a76bdae552a90
Jessica Silverio <jpsilverio1@gmail.com>
2019-01-11 00:14:21 -0500

    Full working version with debug of the seeding with debug data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,30 +36,9 @@ class HomeController extends Controller
 
     public function testa()
     {
-        /*$competences = \Auth::user()->competencies;
-        return view('home', [
-            'competences' => $competences
-        ]); */
-        $stack =[];
-        /*array_unshift($stack,"a");
-        array_unshift($stack,"b");
-        array_unshift($stack,"c");
-        //$stack[] = "a";
-        //$stack[] = "b";
-        //array_pop($stack);
-        //echo $this->array_peek($stack);
-        $elemento = array_shift($stack);
-        echo " oi $elemento<br>";
-
-        $elemento = array_shift($stack);
-        echo " oi $elemento<br>";
-
-        $elemento = array_shift($stack);
-        echo " oi $elemento<br>";
-
-        echo "   olar";*/
         $last_indentation = 0;
         $stack = [];
+        $database_id_stack = [];
         foreach(file(base_path('resources/assets/seeds/teste_seeding.txt')) as $competenceName) {
             $i = substr_count($competenceName,'	');
             echo "que coisa doida $competenceName $i <br>";
@@ -69,14 +48,26 @@ class HomeController extends Controller
                 echo " status = $most / $stackSize <br>";
                 for ($j = 0; $j < $most; $j++) {
                     $elem = array_shift($stack);
+                    $elem_id = array_shift($database_id_stack);
                     echo "removendo $elem <br>";
                 }
             }
             $parent = $this->array_peek($stack);
+            $parent_id = $this->array_peek($database_id_stack);
+            //save to database
+            $competence = new \App\Competency;
+            $competence->name = $competenceName;
+            $competence->description = "olar mundo";
+            if ($parent_id > 0) {
+                $competence->parent_id = $parent_id;
+            }
+            $competence->save();
+            $database_id = $competence->id;
+            array_unshift($database_id_stack,$database_id);
             array_unshift($stack, $competenceName);
             echo " ------------------------------------ <br>";
             echo "nozao: $competenceName" ;
-            echo " || i= $i || last_id = $last_indentation || parent: $parent current stack: <br>";
+            echo " || i= $i || last_id = $last_indentation || parent: $parent || parent database id= $parent_id || database id= $database_id || current stack: <br>";
             print_r($stack);
 
             echo "<br>";
