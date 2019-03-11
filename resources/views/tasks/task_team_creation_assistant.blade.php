790891732113049b51f31f9a1652e5c3e3e8c94d
Jessica Silverio <jpsilverio1@gmail.com>
2019-03-10 21:12:41 -0400

    Displaying candidate ranking in the task team creation assistant

--- resources/views/tasks/task_team_creation_assistant.blade.php
+++ resources/views/tasks/task_team_creation_assistant.blade.php
@@ -12,29 +12,32 @@
                 </div>
         @endif
     <!-- Simple List -->
-    <div style="text-align:center;" class="row">
+    <div  class="row">
         <div class="layer title col-md-3 col-md-offset-1">Candidatos
         </div>
         <div class="layer title col-md-3 col-md-offset-1">Membros da equipe
         </div>
         <div class="layer title col-md-1 col-md-offset-1">Competências
         </div>
+        <div >
+            <ul id="teamCandidates" class="list-group col-md-3 col-md-offset-1" style="background-color: white; min-height: 40px;max-width: 50%; padding:0;">
+                @php ($taskCandidatesInfo = $task->getFinalRankAndExplanations())
 
-        <ul id="teamCandidates" class="list-group col-md-3 col-md-offset-1" style="background-color: white; min-height: 40px;max-width: 50%; padding:0;">
-        @php ($taskCandidatesInfo = $task->getFinalRankAndExplanations())
-
-    @if (count($taskCandidatesInfo["candidates"]) > 0)
-        @foreach ($taskCandidatesInfo["candidates"] as  $candidate)
-            <li class="list-group-item" data-member-source="candidate" accesskey="{{$candidate->id}}">
-                {{$candidate->name}} -> {{$taskCandidatesInfo["ranking"][$candidate->id]}}
-               <div class="glyphicon glyphicon-info-sign task-team-creation-user-popover" data-container="body" data-toggle = "popover" data-placement = "right"  data-html="true"
-                      data-content="
+                @if (count($taskCandidatesInfo["candidates"]) > 0)
+                    @foreach ($taskCandidatesInfo["candidates"] as  $candidate)
+                        <li class="list-group-item" data-member-source="candidate" accesskey="{{$candidate->id}}" type="display: flex; justify-content: space-around;">
+                            <span class="ranking-circle">{{$taskCandidatesInfo["candidateRanking"][$candidate->id]}}</span>
+                            {{$candidate->name}}
+                            <div class="glyphicon glyphicon-info-sign task-team-creation-user-popover" data-container="body" data-toggle = "popover" data-placement = "right"  data-html="true"
+                                 data-content="
                               <h4>Resumo </h4>
                               <ul class='sub-competence'>
-                              <li> Número médio de endossos das competências exigidas pela tarefa:
-                                 {{ $taskCandidatesInfo["rankingData"]["individualRankingValues"]["number of endorsements"][$candidate->id] }}
-                              </li>
-                               <li>  Nível médio de colaboração:
+                              <li> Ranking geral do candidato: {{$taskCandidatesInfo["candidateRanking"][$candidate->id]}}
+                                         </li>
+                                         <li> Número médio de endossos das competências exigidas pela tarefa:
+{{ $taskCandidatesInfo["rankingData"]["individualRankingValues"]["number of endorsements"][$candidate->id] }}
+                                         </li>
+                                          <li>  Nível médio de colaboração:
 {{ number_format($taskCandidatesInfo["rankingData"]["individualRankingValues"]["collaborative competencies"][$candidate->id]*100,2) }}%
                              </li>
                              <li> Nível médio de lembrança das competências exigidas pela tarefa:
@@ -42,39 +45,42 @@
                              </li>
                              <li> Número de competências que o usuário possui e são exigidas pela tarefa:
                                 {{ $taskCandidatesInfo["rankingData"]["individualRankingValues"]["number of competencies"][$candidate->id] }}
-                             </li>
-                             <li> Número de competências que o usuário possui num nivel igual ou superior ao exigido pela tarefa:
-                                {{ $taskCandidatesInfo["rankingData"]["individualRankingValues"]["number of competencies in acceptable level"][$candidate->id] }}
-                             </li>
-                              </ul>
+                                         </li>
+                                         <li> Número de competências que o usuário possui num nivel igual ou superior ao exigido pela tarefa:
+{{ $taskCandidatesInfo["rankingData"]["individualRankingValues"]["number of competencies in acceptable level"][$candidate->id] }}
+                                         </li>
+                                          </ul>
 
-                              <h4> Descrição detalhada </h4>
-                               @foreach($task->competencies as $taskCompetence)
-                      @if(array_key_exists($taskCompetence->id, $taskCandidatesInfo["candidatesContribution"][$candidate->id]["competenceInfo"]))
-                              <hX><b>{{$taskCompetence->name}}</b/></hX>
+                                          <h4> Descrição detalhada </h4>
+@foreach($task->competencies as $taskCompetence)
+                                 @if(array_key_exists($taskCompetence->id, $taskCandidatesInfo["candidatesContribution"][$candidate->id]["competenceInfo"]))
+                                         <hX><b>{{$taskCompetence->name}}</b/></hX>
                               <ul class='sub-competence'>
                               @foreach($taskCandidatesInfo["candidatesContribution"][$candidate->id]["competenceInfo"][$taskCompetence->id]["competence"] as $index => $candidateCompetence)
-                              <li>
-              <i class='fa fa-circle acceptableCompetenceLevel-{{$taskCandidatesInfo["individualCandidateValues"][$candidate->id]["number of competencies in acceptable level"][$candidateCompetence->id]}}' aria-hidden='true'></i>
+                                         <li>
+                         <i class='fa fa-circle acceptableCompetenceLevel-{{$taskCandidatesInfo["individualCandidateValues"][$candidate->id]["number of competencies in acceptable level"][$candidateCompetence->id]}}' aria-hidden='true'></i>
                 {{$candidateCompetence->name}} -
                 {{$candidateCompetence->pivot->proficiency_level_name}} -
                 {{$taskCandidatesInfo["individualCandidateValues"][$candidate->id]["remembering level"][$candidateCompetence->id]}} -
                 {{$taskCandidatesInfo["individualCandidateValues"][$candidate->id]["number of endorsements"][$candidateCompetence->id]}}
-                              </li>
+                                         </li>
 @endforeach
 
-                              </ul>
-                    @endif
-                      @endforeach
+                                         </ul>
+@endif
+                                 @endforeach
 
-                        "></div>
-            </li>
-        @endforeach
-        @else
-        Não há candidatos que possuam as competências requeridas por esta tarefa mas você ainda pode adicionar candidatos manualmente!
-    @endif
+                                         "></div>
+                        </li>
+                    @endforeach
+                @else
+                    Não há candidatos que possuam as competências requeridas por esta tarefa mas você ainda pode adicionar candidatos manualmente!
+                @endif
 
-</ul>
+            </ul>
+        </div>
+
+
 <ul id="candidateTeam" class="list-group col-md-3 col-md-offset-1" style="background-color: white; min-height: 40px;max-width: 50%; padding:0;">
 </ul>
 <div class="col-md-3 col-md-offset-1" style=" max-width: 50%;min-height: 40px; padding:0;">
